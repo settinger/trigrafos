8ba243f21fc9b137def5ef1ff270ca098dd77b14
Sam Ettinger <sam_ettinger@hmc.edu>
2015-05-04 17:58:24 -0400

    Create generateCorpus.py

---
+++ generateCorpus.py
@@ -0,0 +1,35 @@
+# -*- coding: cp1252 -*-
+import re
+import pickle
+from collections import defaultdict
+
+badwords = ['title', 'nonfiltered', 'processed', 'dbindex']
+filenames = ['rawESP/es01', 'rawESP/es02', 'rawESP/es03','rawESP/es04','rawESP/es05','rawESP/es06','rawESP/es07','rawESP/es08','rawESP/es09','rawESP/es10','rawESP/es11','rawESP/es12','rawESP/es13','rawESP/es14','rawESP/es15','rawESP/es16','rawESP/es17','rawESP/es18','rawESP/es19','rawESP/es20','rawESP/es21','rawESP/es22','rawESP/es23','rawESP/es24','rawESP/es25','rawESP/es26','rawESP/es27','rawESP/es28','rawESP/es29','rawESP/es30','rawESP/es31','rawESP/es32','rawESP/es33','rawESP/es34','rawESP/es35','rawESP/es36','rawESP/es37','rawESP/es38','rawESP/es39','rawESP/es40','rawESP/es41','rawESP/es42','rawESP/es43','rawESP/es44','rawESP/es45','rawESP/es46','rawESP/es47','rawESP/es48','rawESP/es49','rawESP/es50','rawESP/es51','rawESP/es52','rawESP/es53','rawESP/es54','rawESP/es55','rawESP/es56','rawESP/es57']
+#filenames = ['rawESP/es01']
+
+# Initialize dicts
+unigram = defaultdict(lambda: 0)
+trigraph = defaultdict(lambda: 0)
+for filename in filenames:
+    # Read in a chunk of the raw corpus
+    with open(filename, 'r') as content_file:
+        content = content_file.read()
+    # Tokenize a chunk of the raw corpus
+    pattern = ur'[ ][a-záéíóúñü]+'
+    rawtokens = re.findall(pattern, content)
+    tokens = [token[1:] for token in rawtokens if len(token)>5 and token[1:] not in badwords]
+    # Create unigram, trigraph models
+    for token in tokens:
+        unigram[token] += 1
+        for num in range(len(token)-2):
+            trigraph[token[num:num+3]] += 1
+    print('Finished with ' + filename)
+
+out1 = open('unigram.dict', 'w')
+pickle.dump(dict(unigram), out1)
+out1.close()
+out2 = open('trigraph.dict','w')
+pickle.dump(dict(trigraph), out2)
+out2.close()
+
+#áéíóúñüÁÉÍÓÚÑÜ
