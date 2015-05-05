cb15b32b3e1c4f1885e9733664e5d17d1124b417
Sam Ettinger <{u'verified': True, u'email': u'sam_ettinger@hmc.edu', u'primary': True}>
2015-05-05 17:20:18 -0400

    Changed trigraph storage, added difficulty levels, hopefully fixed encoding woes

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 A word game! If you are given an arbitrary three-letter string, can you come up with a word that includes that string? Right now only in Spanish.
 
 Trígrafos is meant as a game for foreign language vocabulary stretching, so this repo features both game code and corpus-processing code (for making new language versions).
+
+## How to run the game
+Make sure you have main.py, levels.list, and unigram.dict in the same folder, then open main.py. If you have Python 2.something, it should run!
+
+## How to play the game
+The game will give you three letters, such as 'acu,' and prompts to you give a Spanish word that contains those letters in order (e.g. 'lacuna', 'acuesta'). You may want to have the special characters áéíóúñü handy! Also, your word must be five letters or more, just because. The better you do, the harder it gets (that is, the rarer the trigraphs become).
+
+## Various notes
+The Spanish frequency data is gleaned from WikiCorpus Español, which is roughly 70 million tokens taken from the Spanish-language Wikipedia. As such, there are some flaws---for example, it may want you to find words that contain 'kéb' (answers: pokéball(s), pokéblock(s)). The letter pairs 'rr', 'll', and 'ch' are counted as two letters each. This approach won't always work; for example, the Catalan 'l·l' really has to be treated as a unigraph. The script I used to generate the Spanish trigraph frequencies is included but it sucks.
--- generateCorpus.py
+++ generateCorpus.py
@@ -1,7 +1,8 @@
-# -*- coding: cp1252 -*-
+# -*- coding: utf-8 -*-
 import re
 import pickle
 from collections import defaultdict
+from math import log10, ceil
 
 badwords = ['title', 'nonfiltered', 'processed', 'dbindex']
 filenames = ['../rawESP/es01', '../rawESP/es02', '../rawESP/es03', '../rawESP/es04', '../rawESP/es05', '../rawESP/es06', '../rawESP/es07', '../rawESP/es08', '../rawESP/es09', '../rawESP/es10', '../rawESP/es11', '../rawESP/es12', '../rawESP/es13', '../rawESP/es14', '../rawESP/es15', '../rawESP/es16', '../rawESP/es17', '../rawESP/es18', '../rawESP/es19', '../rawESP/es20', '../rawESP/es21', '../rawESP/es22', '../rawESP/es23', '../rawESP/es24', '../rawESP/es25', '../rawESP/es26', '../rawESP/es27', '../rawESP/es28', '../rawESP/es29', '../rawESP/es30', '../rawESP/es31', '../rawESP/es32', '../rawESP/es33', '../rawESP/es34', '../rawESP/es35', '../rawESP/es36', '../rawESP/es37', '../rawESP/es38', '../rawESP/es39', '../rawESP/es40', '../rawESP/es41', '../rawESP/es42', '../rawESP/es43', '../rawESP/es44', '../rawESP/es45', '../rawESP/es46', '../rawESP/es47', '../rawESP/es48', '../rawESP/es49', '../rawESP/es50', '../rawESP/es51', '../rawESP/es52', '../rawESP/es53', '../rawESP/es54', '../rawESP/es55', '../rawESP/es56', '../rawESP/es57']
@@ -15,7 +16,7 @@ for filename in filenames:
     with open(filename, 'r') as content_file:
         content = content_file.read()
     # Tokenize a chunk of the raw corpus
-    pattern = ur'[ ][a-záéíóúñü]+'
+    pattern = ur'[ (][a-zÃ¡Ã©Ã­Ã³ÃºÃ±Ã¼]+'
     rawtokens = re.findall(pattern, content)
     tokens = [token[1:] for token in rawtokens if len(token)>5 and token[1:] not in badwords]
     # Create unigram, trigraph models
@@ -30,7 +31,7 @@ for filename in filenames:
 # Finish the unigram model by converting to a regular dict
 unigram = dict(unigram)
 
-# Finish the trigraph model by converting AND ALSO keeping only five tokens that contain the trigraph
+# Edit the trigraph model by converting AND ALSO keeping only five tokens that contain the trigraph
 trigraph = dict(trigraph)
 n = 0.0
 N = float(len(trigraph.keys()))
@@ -46,13 +47,35 @@ for key in trigraph.keys():
     n += 1.0
     print(str(n/N)[0:6] + ' done with trigraph LM')
 
+# Break trigraph dict into N dicts, one for each difficulty level
+N = 20   # Let there be 20 difficulty levels
+cut = 5  # Ignore any trigrams that appear fewer than 5 times
+topCount = max([trigraph[key][0] for key in trigraph.keys()])  # Count of most frequent trigraph
+# trigraph goes into one of N sub-dicts, depending on its count
+
+def whichDict(N, cut, topCount, count):
+    ''' Dictates which level of difficulty is appropriate for a certain trigraph'''
+    spread = (log10(topCount) - log10(cut))/float(N)  # Log range of one difficulty level
+    diff = int(ceil((log10(count) - log10(cut))/spread))
+    return diff-1
+# If whichdict(trigraph count) is between 1 and N, assign it to the appropriate subdict
+# Initialize N dicts
+levels = [dict() for num in range(N)]
+for key in trigraph.keys():
+    if trigraph[key][0] > cut:
+        index = whichDict(N, cut, topCount, trigraph[key][0])
+        levels[index][key] = trigraph[key]
+
 print('Writing .dict files...')
 
 out1 = open('unigram.dict', 'w')
 pickle.dump(unigram, out1)
 out1.close()
-out2 = open('trigraph.dict','w')
-pickle.dump(dict(trigraph), out2)
+out2 = open('trigraph.dict','w') # No longer necessary!
+pickle.dump(trigraph, out2)
 out2.close()
+out3 = open('levels.list','w')
+pickle.dump(levels, out3)
+out3.close()
 
-#áéíóúñüÁÉÍÓÚÑÜ'''
+#Ã¡Ã©Ã­Ã³ÃºÃ±Ã¼ÃÃ‰ÃÃ“ÃšÃ‘Ãœ'''
